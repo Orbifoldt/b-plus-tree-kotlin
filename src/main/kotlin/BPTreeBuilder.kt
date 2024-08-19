6cd65632ec034dc5411699747a0e1c019bb98c9d
Orbifoldt <eikenrode02@gmail.com>
2024-08-19 21:42:42 +0200

    Finalizing the BPTree builder DSL

--- src/main/kotlin/BPTreeBuilder.kt
+++ src/main/kotlin/BPTreeBuilder.kt
@@ -1,48 +1,81 @@
 package org.example
 
-
-fun main(){
-    val x = bPTree(degree = 3, 8) {
-        internal(4) {
-            internal(2) {
-                leaf(0 to "0")
-                leaf { keyValue(2, "2") }
-            }
-            internal(6) {
-                leaf(4 to "4", 5 to "5")
-                leaf {
-                    keyValue(6, "6")
-                    keyValue(7, "7")
-                }
-            }
-        }
-        internal(12) {
-            internal(10) {
-                leaf { keyValue(8, "8") }
-                leaf { keyValue(10, "10") }
-            }
-            internal(14) {
-                leaf { keyValue(12, "12") }
-                leaf { keyValue(14, "14") }
-            }
-        }
-    }
-    println(x)
-}
-
-fun bPTree(degree: Int, vararg keys: Int, f: BPTreeBuilder.() -> Unit) =
+/**
+ * Builds a new B+ Tree of degree [degree] by creating a root node with the specified [keys], and populating it using
+ * the given [builderAction].
+ *
+ * Example:
+ * ```kotlin
+ * val tree = buildBPTree(degree = 3, 8) {
+ *         internal(4) {
+ *             internal(1) {
+ *                 leaf(0 to "0")
+ *                 leaf(2 to "2")
+ *             }
+ *             internal(6) {
+ *                 leaf(4 to "4", 5 to "5")
+ *                 leaf(6 to "6", 7 to "7")
+ *             }
+ *         }
+ *         internal(12) {
+ *             internal(10) {
+ *                 leaf {  // alternative syntax
+ *                     keyValue(8 to "8")
+ *                     keyValue(9 to "9")
+ *                 }
+ *                 leaf(10 to "10")
+ *             }
+ *             internal(14) {
+ *                 leaf(12 to "12")
+ *                 leaf(14 to "14")
+ *             }
+ *         }
+ *     }
+ * ```
+ *
+ * @throws IllegalArgumentException when the provided [keys] and [builderAction] would create an invalid B+ Tree of
+ * the provided [degree].
+ */
+fun buildBPTree(degree: Int, vararg keys: Int, builderAction: BPTreeBuilder.() -> Unit) =
     BPTreeBuilder(degree, *keys)
-        .apply(f)
+        .apply(builderAction)
         .build()
 
 class BPTreeBuilder internal constructor(private val degree: Int, vararg keys: Int) {
     private val root = InternalNodeBuilder(degree, *keys, isRoot = true)
 
-    fun build() = BPTree(degree, root.build())
+    fun internal(vararg keys: Int, childrenBuilderAction: InternalNodeBuilder.() -> Unit) =
+        root.internal(*keys, childrenBuilderAction = childrenBuilderAction)
 
-    fun internal(vararg keys: Int, f: InternalNodeBuilder.() -> Unit){
-        root.internal(*keys) {
-            f()
+    fun leaf(leafBuilderAction: LeafBuilder.() -> Unit) = root.leaf(leafBuilderAction)
+
+    fun leaf(vararg keyValues: Pair<Int, String>) = root.leaf(*keyValues)
+
+    fun build(): BPTree {
+        val rootNode = try {
+            root.build()
+        } catch (e: IllegalStateException) {
+            throw IllegalArgumentException(e.message, e)
+        }
+        setNeighborsForAllLayers(rootNode)
+        return BPTree(degree, rootNode)
+    }
+
+    private fun setNeighborsForAllLayers(rootNode: InternalNode) {
+        var currentLayer = ArrayDeque<Node>(listOf(rootNode))
+        var nextLayer = ArrayDeque<Node>()
+        while (currentLayer.isNotEmpty()) {
+            currentLayer.zipWithNext().forEach { (left, right) ->
+                if (left is LeafNode && right is LeafNode) {
+                    left.rightNeighbor = right; right.leftNeighbor = left
+                } else if (left is InternalNode && right is InternalNode) {
+                    left.rightNeighbor = right; right.leftNeighbor = left
+                } else throw IllegalArgumentException("Expected all children to be of same type")
+            }
+
+            currentLayer.filterIsInstance<InternalNode>().forEach { nextLayer.addAll(it.children) }
+            currentLayer = nextLayer
+            nextLayer = ArrayDeque()
         }
     }
 }
@@ -55,34 +88,32 @@ class InternalNodeBuilder internal constructor(
     private val degree: Int,
     vararg keys: Int,
     private val isRoot: Boolean = false,
-): NodeBuilder {
+) : NodeBuilder {
     private val keys = mutableListOf<Int>().apply { addAll(keys.toList()) }
     private val children = mutableListOf<NodeBuilder>()
 
-    fun internal(vararg keys: Int, f: InternalNodeBuilder.() -> Unit){
+    fun internal(vararg keys: Int, childrenBuilderAction: InternalNodeBuilder.() -> Unit) {
         children += InternalNodeBuilder(degree, *keys)
-            .apply(f)
+            .apply(childrenBuilderAction)
     }
 
-    fun leaf(f: LeafBuilder.() -> Unit){
-        this.children += LeafBuilder(degree).apply(f)
+    fun leaf(leafBuilderAction: LeafBuilder.() -> Unit) {
+        children += LeafBuilder(degree).apply(leafBuilderAction)
     }
 
-    fun leaf(vararg keyValuePairs: Pair<Int, String>){
-        children.add(LeafBuilder(degree, keyValuePairs.toList()))
+    fun leaf(vararg keyValues: Pair<Int, String>) {
+        children += LeafBuilder(degree, keyValues.toList())
     }
 
     override fun build(): InternalNode {
-        val children: MutableList<Node> = if(children.all { it is InternalNodeBuilder }){
-             children.filterIsInstance<InternalNodeBuilder>()  // to please the compiler
-                 .map(InternalNodeBuilder::build)
-                 .also { it.zipWithNext { left, right -> left.rightNeighbor = right; right.leftNeighbor = left } }
-                 .toMutableList()
-        } else if(children.all { it is LeafBuilder }){
+        val children: MutableList<Node> = if (children.all { it is InternalNodeBuilder }) {
+            children.filterIsInstance<InternalNodeBuilder>()
+                .map(InternalNodeBuilder::build)
+                .toMutableList()
+        } else if (children.all { it is LeafBuilder }) {
             children.filterIsInstance<LeafBuilder>()  // to please the compiler
-                 .map(LeafBuilder::build)
-                 .also { it.zipWithNext { left, right -> left.rightNeighbor = right; right.leftNeighbor = left } }
-                 .toMutableList()
+                .map(LeafBuilder::build)
+                .toMutableList()
         } else throw IllegalArgumentException("Node may only contain one type of nodes, found both internal and leaves")
 
         return InternalNode(keys, children).also { it.checkIsValid(degree, isRoot) }
@@ -91,18 +122,19 @@ class InternalNodeBuilder internal constructor(
 
 class LeafBuilder internal constructor(
     private val degree: Int,
-    keyValuePairs: List<Pair<Int, String>> = emptyList(),
-): NodeBuilder {
-    private val keys = mutableListOf<Int>().apply { addAll(keyValuePairs.map { (key, _) -> key }) }
-    private val values = mutableListOf<String>().apply { addAll(keyValuePairs.map { (_, value) -> value }) }
+    keyValues: List<Pair<Int, String>> = emptyList(),
+) : NodeBuilder {
+    private val keyValuePairs = mutableListOf<Pair<Int, String>>().apply { addAll(keyValues) }
 
-    fun keyValue(key: Int, value: String){
-        keys.add(key)
-        values.add(value)
+    infix fun keyValue(keyValue: Pair<Int, String>) {
+        keyValuePairs.add(keyValue)
     }
 
     override fun build(): LeafNode {
-        keys.sort()
-        return LeafNode(keys, values).also { it.checkIsValid(degree, false) }
+        return LeafNode(
+            keys = keyValuePairs.map { it.first }.toMutableList(),
+            values = keyValuePairs.map { it.second }.toMutableList(),
+        )
+            .also { it.checkIsValid(degree, false) }
     }
 }
---
+++ src/test/kotlin/BPTreeBuilderTest.kt
@@ -0,0 +1,106 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.example.buildBPTree
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class BPTreeBuilderTest {
+    private val tree = buildBPTree(degree = 3, 8) {
+        internal(4) {
+            internal(1) {
+                leaf(0 to "0")
+                leaf(2 to "2")
+            }
+            internal(6) {
+                leaf(4 to "4", 5 to "5")
+                leaf(6 to "6", 7 to "7")
+            }
+        }
+        internal(12) {
+            internal(10) {
+                leaf {  // alternative syntax
+                    keyValue(8 to "8")
+                    keyValue(9 to "9")
+                }
+                leaf(10 to "10")
+            }
+            internal(14) {
+                leaf(12 to "12")
+                leaf(14 to "14")
+            }
+        }
+    }
+
+    @Test
+    fun `Should be able to fetch values from the created tree`() {
+        assertThat(tree.get(2)).isEqualTo("2")
+        assertThat(tree.get(6)).isEqualTo("6")
+        assertThat(tree.get(7)).isEqualTo("7")
+        assertThat(tree.get(8)).isEqualTo("8")
+        assertThat(tree.get(3)).isNull()
+        assertThat(tree.get(666)).isNull()
+    }
+
+    @Test
+    fun `Should be able to list all values in the tree`() {
+        // This also verifies that the neighbor are set correctly
+        assertThat(tree.values).containsExactly("0", "2", "4", "5", "6", "7", "8", "10", "12", "14")
+    }
+
+    @Test
+    fun `Should be able to delete values from the created tree`() {
+        assertThat(tree.remove(4)).isEqualTo("4")
+        assertThat(tree.remove(7)).isEqualTo("7")
+        assertThat(tree.remove(3)).isNull()
+        assertThat(tree.remove(4)).isNull()
+        assertThat(tree.remove(0)).isEqualTo("0")
+        assertThat(tree.values).containsExactly("2", "5", "6", "8", "10", "12", "14")
+    }
+
+    @Nested
+    inner class `While creating an invalid tree, should throw IllegalArgumentException` {
+        @Test
+        fun `when number of keys doesn't match number of children`(){
+            assertThatThrownBy {
+                buildBPTree(degree = 4, 3, 5) {
+                    leaf(0 to "0")
+                }
+            }.isInstanceOf(IllegalArgumentException::class.java)
+        }
+
+        @Test
+        fun `when a mix of leaf and internal nodes is provided`(){
+            assertThatThrownBy {
+                buildBPTree(degree = 3, 8) {
+                    leaf(6 to "6")
+                    internal(9) {
+                        leaf(8 to "8")
+                        leaf(33 to "33")
+                    }
+                }
+            }.isInstanceOf(IllegalArgumentException::class.java)
+        }
+
+        @Test
+        fun `when number of values per leaf is too low for the degree`(){
+            assertThatThrownBy {
+                buildBPTree(degree = 5, 8, 33) {
+                    leaf(6 to "6")
+                    leaf(8 to "8")
+                    leaf(33 to "33")
+                }
+            }.isInstanceOf(IllegalArgumentException::class.java)
+        }
+
+        @Test
+        fun `when the keys are not sorted correctly`(){
+            assertThatThrownBy {
+                buildBPTree(degree = 3, 33, 8) {
+                    leaf(6 to "6")
+                    leaf(8 to "8")
+                    leaf(33 to "33")
+                }
+            }.isInstanceOf(IllegalArgumentException::class.java)
+        }
+    }
+}
